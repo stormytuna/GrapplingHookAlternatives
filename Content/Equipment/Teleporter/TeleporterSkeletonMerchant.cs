16e6d6ae40e2767671b374ba5153258d70dee966
stormytuna <stormytuna@outlook.com>
2024-04-01 13:45:48 +0100

    Finalise teleporter

---
+++ Content/Equipment/Teleporter/TeleporterSkeletonMerchant.cs
@@ -0,0 +1,14 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GrapplingHookAlternatives.Content.Equipment.Teleporter;
+
+public class TeleporterSkeletonMerchant : GlobalNPC
+{
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.SkeletonMerchant;
+
+    public override void ModifyShop(NPCShop shop) {
+        shop.Add<Teleporter>(Condition.MoonPhaseNew, Condition.Hardmode);
+    }
+}
